a70f1724cd7f49c2e48af6ffeb771352c6b478bb
Tushar Srivastava <srivastavashishir23@gmail.com>
2026-01-24 13:50:08 +0530

    hi

--- Age calculator.py
+++ Age calculator.py
@@ -4,4 +4,4 @@ dob_input = input("Enter your date of birth (YYYY-MM-DD): ")
 dob = datetime.strptime(dob_input, "%Y-%m-%d").date()
 print(dob)
 age = [today.year - dob.year , today.month - dob.month , today.day - dob.day]
-print(f" you are {age} year old")
+print(f" you  {age} year old")
